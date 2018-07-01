814ae645ab25068f81cbffa7a81a904213bd8766
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-01 05:09:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,3 @@ if katz_deli.empty?
   puts "The line is currently empty."
 end
    puts"The line is currently: "
-katz_deli.each do|name|
-  counter= 1
- puts  "#{counter}. #{name}"
-  counter+=1
-end
-end
