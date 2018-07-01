3f17a9391bfaad880a6d7c301a1eac6e1f5f0471
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-01 05:25:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,6 @@ end
 end
 def take_a_number(katz_deli, name)
   katz_deli  << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line. 
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   
 end
