9d2117481b626973a80fd7d6a5623c9b158aff0e
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-01 05:16:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,11 @@ def line(katz_deli)
 if katz_deli.empty?
   puts "The line is currently empty."
 else
-   puts"The line is currently: "
+   message= "The line is currently: "
    
 end
   katz_deli.each_with_index do |value, index|
-      puts " #{index.to_i+1}. #{value}"
+    message +=" #{index.to_i+1}. #{value}"
 end
+puts message
 end
